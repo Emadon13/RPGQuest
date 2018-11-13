7ff876541c7c1187cc55a50d68111f620a6f4101
Emadon13 <38349700+Emadon13@users.noreply.github.com>
2018-11-13 17:10:07 +0100

    deprecia

---
+++ Depressia/main.cpp
@@ -0,0 +1,36 @@
+/*
+#include <QApplication>
+#include <QPushButton>
+#include <iostream>
+#include <iostream>
+#include <fstream>
+#include "io/entityloader.h"
+#include "gui/globalspec.h"
+
+using namespace std;
+
+GlobalSpec * globalSpec;
+
+int main(int argc, char *argv[])
+{
+
+    // ------------ TEST VINCENT -----------------
+
+    EntityLoader el;
+    Entity e = el.generate("../ressources/goblin.txt");
+    cout << e.getName() << endl;
+
+    // ------------TEST QUENTIN ------------------
+    QApplication app (argc, argv);
+    QApplication::setFont(QFont("Fixedsys"));
+
+    globalSpec = new GlobalSpec();
+
+    //set up start Screen
+    StartScreen * start = new StartScreen(*global);
+    start->show();
+
+    return app.exec();
+//t bo
+}
+*/
