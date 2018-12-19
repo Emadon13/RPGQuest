da4336556bcd63d34f53b0b5eb8e4de049445157
Emadon13 <38349700+Emadon13@users.noreply.github.com>
2018-12-19 14:51:04 +0100

    zertyuik

--- Depressia/logic/items/healitem.cpp
+++ Depressia/logic/items/healitem.cpp
@@ -1,6 +1,27 @@
 #include "healitem.h"
 
-HealItem::HealItem()
+HealItem::HealItem():
+    Item(),
+    hpHeal(10)
 {
 
 }
+
+HealItem::HealItem(std::string n, std::string d, std::string i, int hp):
+    Item(n, d, i),
+    hpHeal(hp)
+{
+    
+}
+
+HealItem::~HealItem()
+{
+    
+}
+/*
+std::string HealItem::use(Entity* user, Entity* target)
+{
+    target->restaureHp(hpHeal);
+    return
+}
+*/
