d6d091e0a7e2942ff8a5113cf978f1212d3cf3cf
Emadon13 <38349700+Emadon13@users.noreply.github.com>
2019-01-07 11:12:01 +0100

    dfghjkl

--- Depressia/logic/events/save.h
+++ Depressia/logic/events/save.h
@@ -2,12 +2,15 @@
 #define SAVE_H
 
 #include "logic/events/event.h"
-
+#include "io/gameloader.h"
 
 class Save : public Event
 {
 public:
     Save();
+
+    void heal(Team team);
+    void save(Game game);
 };
 
 #endif // SAVE_H
