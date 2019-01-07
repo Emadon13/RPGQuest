111d34ba9cdcba53107a2735ce235fdbdf3ab520
Emadon13 <38349700+Emadon13@users.noreply.github.com>
2019-01-07 19:26:40 +0100

    zjbibevm

--- build-Depressia-Vincent/debug/moc_gamewindow.cpp
+++ build-Depressia-Vincent/debug/moc_gamewindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_GameWindow_t {
-    QByteArrayData data[19];
-    char stringdata0[249];
+    QByteArrayData data[24];
+    char stringdata0[314];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -39,26 +39,33 @@ QT_MOC_LITERAL(4, 40, 15), // "CreateGameFrame"
 QT_MOC_LITERAL(5, 56, 17), // "CreateDialogFrame"
 QT_MOC_LITERAL(6, 74, 16), // "CreateVideoFrame"
 QT_MOC_LITERAL(7, 91, 16), // "CreateFinalFrame"
-QT_MOC_LITERAL(8, 108, 19), // "CreateGameOverFrame"
-QT_MOC_LITERAL(9, 128, 15), // "CreateItemFrame"
-QT_MOC_LITERAL(10, 144, 4), // "GoUp"
-QT_MOC_LITERAL(11, 149, 6), // "GoDown"
-QT_MOC_LITERAL(12, 156, 7), // "GoRight"
-QT_MOC_LITERAL(13, 164, 6), // "GoLeft"
-QT_MOC_LITERAL(14, 171, 11), // "afficheItem"
-QT_MOC_LITERAL(15, 183, 15), // "afficheItemList"
-QT_MOC_LITERAL(16, 199, 16), // "afficheParametre"
-QT_MOC_LITERAL(17, 216, 10), // "resetFocus"
-QT_MOC_LITERAL(18, 227, 21) // "setPositionAfterLoose"
+QT_MOC_LITERAL(8, 108, 18), // "CreateAddHeroFrame"
+QT_MOC_LITERAL(9, 127, 15), // "CreateSaveFrame"
+QT_MOC_LITERAL(10, 143, 19), // "CreateGameOverFrame"
+QT_MOC_LITERAL(11, 163, 15), // "CreateItemFrame"
+QT_MOC_LITERAL(12, 179, 4), // "GoUp"
+QT_MOC_LITERAL(13, 184, 6), // "GoDown"
+QT_MOC_LITERAL(14, 191, 7), // "GoRight"
+QT_MOC_LITERAL(15, 199, 6), // "GoLeft"
+QT_MOC_LITERAL(16, 206, 11), // "afficheSave"
+QT_MOC_LITERAL(17, 218, 11), // "afficheItem"
+QT_MOC_LITERAL(18, 230, 15), // "afficheItemList"
+QT_MOC_LITERAL(19, 246, 16), // "afficheParametre"
+QT_MOC_LITERAL(20, 263, 10), // "resetFocus"
+QT_MOC_LITERAL(21, 274, 21), // "setPositionAfterLoose"
+QT_MOC_LITERAL(22, 296, 8), // "makeSave"
+QT_MOC_LITERAL(23, 305, 8) // "healTeam"
 
     },
     "GameWindow\0ShowFrame\0\0CreateBattleFrame\0"
     "CreateGameFrame\0CreateDialogFrame\0"
     "CreateVideoFrame\0CreateFinalFrame\0"
+    "CreateAddHeroFrame\0CreateSaveFrame\0"
     "CreateGameOverFrame\0CreateItemFrame\0"
-    "GoUp\0GoDown\0GoRight\0GoLeft\0afficheItem\0"
-    "afficheItemList\0afficheParametre\0"
-    "resetFocus\0setPositionAfterLoose"
+    "GoUp\0GoDown\0GoRight\0GoLeft\0afficheSave\0"
+    "afficheItem\0afficheItemList\0"
+    "afficheParametre\0resetFocus\0"
+    "setPositionAfterLoose\0makeSave\0healTeam"
 };
 #undef QT_MOC_LITERAL
 
@@ -68,7 +75,7 @@ static const uint qt_meta_data_GameWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      17,   14, // methods
+      22,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -76,25 +83,35 @@ static const uint qt_meta_data_GameWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   99,    2, 0x0a /* Public */,
-       3,    0,  100,    2, 0x0a /* Public */,
-       4,    0,  101,    2, 0x0a /* Public */,
-       5,    0,  102,    2, 0x0a /* Public */,
-       6,    0,  103,    2, 0x0a /* Public */,
-       7,    0,  104,    2, 0x0a /* Public */,
-       8,    0,  105,    2, 0x0a /* Public */,
-       9,    0,  106,    2, 0x0a /* Public */,
-      10,    0,  107,    2, 0x0a /* Public */,
-      11,    0,  108,    2, 0x0a /* Public */,
-      12,    0,  109,    2, 0x0a /* Public */,
-      13,    0,  110,    2, 0x0a /* Public */,
-      14,    0,  111,    2, 0x0a /* Public */,
-      15,    0,  112,    2, 0x0a /* Public */,
-      16,    0,  113,    2, 0x0a /* Public */,
-      17,    0,  114,    2, 0x0a /* Public */,
-      18,    0,  115,    2, 0x0a /* Public */,
+       1,    0,  124,    2, 0x0a /* Public */,
+       3,    0,  125,    2, 0x0a /* Public */,
+       4,    0,  126,    2, 0x0a /* Public */,
+       5,    0,  127,    2, 0x0a /* Public */,
+       6,    0,  128,    2, 0x0a /* Public */,
+       7,    0,  129,    2, 0x0a /* Public */,
+       8,    0,  130,    2, 0x0a /* Public */,
+       9,    0,  131,    2, 0x0a /* Public */,
+      10,    0,  132,    2, 0x0a /* Public */,
+      11,    0,  133,    2, 0x0a /* Public */,
+      12,    0,  134,    2, 0x0a /* Public */,
+      13,    0,  135,    2, 0x0a /* Public */,
+      14,    0,  136,    2, 0x0a /* Public */,
+      15,    0,  137,    2, 0x0a /* Public */,
+      16,    0,  138,    2, 0x0a /* Public */,
+      17,    0,  139,    2, 0x0a /* Public */,
+      18,    0,  140,    2, 0x0a /* Public */,
+      19,    0,  141,    2, 0x0a /* Public */,
+      20,    0,  142,    2, 0x0a /* Public */,
+      21,    0,  143,    2, 0x0a /* Public */,
+      22,    0,  144,    2, 0x0a /* Public */,
+      23,    0,  145,    2, 0x0a /* Public */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -128,17 +145,22 @@ void GameWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 3: _t->CreateDialogFrame(); break;
         case 4: _t->CreateVideoFrame(); break;
         case 5: _t->CreateFinalFrame(); break;
-        case 6: _t->CreateGameOverFrame(); break;
-        case 7: _t->CreateItemFrame(); break;
-        case 8: _t->GoUp(); break;
-        case 9: _t->GoDown(); break;
-        case 10: _t->GoRight(); break;
-        case 11: _t->GoLeft(); break;
-        case 12: _t->afficheItem(); break;
-        case 13: _t->afficheItemList(); break;
-        case 14: _t->afficheParametre(); break;
-        case 15: _t->resetFocus(); break;
-        case 16: _t->setPositionAfterLoose(); break;
+        case 6: _t->CreateAddHeroFrame(); break;
+        case 7: _t->CreateSaveFrame(); break;
+        case 8: _t->CreateGameOverFrame(); break;
+        case 9: _t->CreateItemFrame(); break;
+        case 10: _t->GoUp(); break;
+        case 11: _t->GoDown(); break;
+        case 12: _t->GoRight(); break;
+        case 13: _t->GoLeft(); break;
+        case 14: _t->afficheSave(); break;
+        case 15: _t->afficheItem(); break;
+        case 16: _t->afficheItemList(); break;
+        case 17: _t->afficheParametre(); break;
+        case 18: _t->resetFocus(); break;
+        case 19: _t->setPositionAfterLoose(); break;
+        case 20: _t->makeSave(); break;
+        case 21: _t->healTeam(); break;
         default: ;
         }
     }
@@ -170,13 +192,13 @@ int GameWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 17)
+        if (_id < 22)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 17;
+        _id -= 22;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 17)
+        if (_id < 22)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 17;
+        _id -= 22;
     }
     return _id;
 }
